97e964973a575c30fda228ff2be4ac18e6aa5ce6
Android <l.cardoso64@hotmail.com>
2024-12-20 21:45:52 -0300

    Adiciona leitura de arquivo CSV e tratamento de exceções no ControleComandas

--- ControleDeComandas/src/ControleComandas.java
+++ ControleDeComandas/src/ControleComandas.java
@@ -1,13 +1,47 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+// import java.util.*;
 
 public class ControleComandas {
 
     public static void testeLeitura() {
         // 1 encontrar o arquivo
-        File arquivo = new File("~/android/devFolder/POO22024/ControleDeComandas/src/refeicoes_e_bebidas.csv");
+        File arquivo = new File("ControleDeComandas/src/refeicoes_e_bebidas.csv");
 
         // 2 marcar como leitura
+        if (arquivo.exists() && arquivo.canRead() && arquivo.isFile()) {
+            System.out.println("Arquivo encontrado e pode ser lido");
+            try {
+                FileReader marcarLeitura = new FileReader(arquivo);
+                BufferedReader bufLeitura = new BufferedReader(marcarLeitura);
+
+                String linha = bufLeitura.readLine(); // captura uma linha
+                System.out.println(linha); // imprimir a linha
+
+                linha = bufLeitura.readLine(); // captura uma linha
+                System.out.println(linha); // imprimir a linha
+
+                linha = bufLeitura.readLine(); // captura uma linha
+                System.out.println(linha); // imprimir a linha
+
+                linha = bufLeitura.readLine(); // captura uma linha
+                System.out.println(linha); // imprimir a linha
+
+
+
+            } catch (FileNotFoundException erro) {
+                System.out.println(("Caminho do arquivo incorreto."));
+            } catch (IOException erroLeitura) {
+                System.out.println("Erro ao ler o arquivo.");
+            }
+
+        } else {
+            System.out.println("Arquivo não encontrado ou não pode ser lido");
+            return;
+        }
 
         // 3 ler linhas do arquivo
     }
