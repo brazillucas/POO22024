d7d2708b3c643482be54c875847443e2219b0b77
Android <l.cardoso64@hotmail.com>
2024-12-29 17:20:24 -0300

    Atualiza a inicialização do tabuleiro adicionando leitura de arquivo CSV ao invés de fazer manual.

--- BancoIF/src/Tabuleiro.java
+++ BancoIF/src/Tabuleiro.java
@@ -23,6 +23,10 @@
 | Cadeia            | Avenida 9 Julho | Avenida Rebouças | Companhia de Viação | Avenida Brigadeiro Faria Lima | Companhia Ferroviária  | Avenida Nossa Senhora de Copacabana | Avenida Presidente Vargas | Sorte/Reves   | Leblon        | Partida - Receba (+200)   |
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Tabuleiro {
     // Atributos e métodos para gerenciar o tabuleiro
     private Posicao[] posicoes;
@@ -33,47 +37,40 @@ public class Tabuleiro {
     }
 
     private void inicializarTabuleiro() {
-        // Preenchendo as posições do tabuleiro.
-        posicoes[0] = new Posicao("Partida", "Inicial");
-        posicoes[1] = new Propriedade("Bangu", 100, 10, 50, 100);
-        posicoes[2] = new CartaDeSorte("Sorte/Reves");
-        posicoes[3] = new Propriedade("Botafogo", 200, 20, 100, 200);
-        posicoes[4] = new ImpostoDeRenda("Imposto de Renda");
-        posicoes[5] = new Companhia("Companhia de Navegação", 150, 4);
-        posicoes[6] = new Propriedade("Avenida Brasil", 300, 30, 150, 300);
-        posicoes[7] = new CartaDeSorte("Sorte/Reves");
-        posicoes[8] = new Propriedade("Avenida Paulista", 400, 40, 200, 400);
-        posicoes[9] = new Propriedade("Jardim Europa", 500, 50, 250, 500);
-        posicoes[10] = new Cadeia("Vá para a Prisão");
-        posicoes[11] = new Propriedade("Copacabana", 600, 60, 300, 600);
-        posicoes[12] = new Companhia("Companhia de Aviação", 150, 4);
-        posicoes[13] = new Propriedade("Avenida Vieira Souto", 700, 70, 350, 700);
-        posicoes[14] = new Propriedade("Avenida Atlântica", 800, 80, 400, 800);
-        posicoes[15] = new Companhia("Companhia Táxi Aéreo", 150, 4);
-        posicoes[16] = new Propriedade("Ipanema", 900, 90, 450, 900);
-        posicoes[17] = new CartaDeSorte("Sorte/Reves");
-        posicoes[18] = new Propriedade("Jardim Paulista", 1000, 100, 500, 1000);
-        posicoes[19] = new Propriedade("Brooklin", 1100, 110, 550, 1100);
-        posicoes[20] = new Cadeia("Cadeia");
-        posicoes[21] = new Propriedade("Avenida 9 de Julho", 1200, 120, 600, 1200);
-        posicoes[22] = new Propriedade("Avenida Rebouças", 1300, 130, 650, 1300);
-        posicoes[23] = new Companhia("Companhia de Viação", 150, 4);
-        posicoes[24] = new Propriedade("Avenida Brigadeiro Faria Lima", 1400, 140, 700, 1400);
-        posicoes[25] = new Companhia("Companhia Ferroviária", 150, 4);
-        posicoes[26] = new Propriedade("Avenida Nossa Senhora de Copacabana", 1500, 150, 750, 1500);
-        posicoes[27] = new Propriedade("Avenida Presidente Vargas", 1600, 160, 800, 1600);
-        posicoes[28] = new CartaDeSorte("Sorte/Reves");
-        posicoes[29] = new Propriedade("Leblon", 1700, 170, 850, 1700);
-        posicoes[30] = new Cadeia("Vá para a Prisão");
-        posicoes[31] = new Propriedade("Morumbi", 1800, 180, 900, 1800);
-        posicoes[32] = new LucrosOuDividendos("Lucros ou Dividendos");
-        posicoes[33] = new Propriedade("Interlagos", 1900, 190, 950, 1900);
-        posicoes[34] = new CartaDeSorte("Sorte/Reves");
-        posicoes[35] = new Companhia("Companhia de Táxi", 150, 4);
-        posicoes[36] = new Propriedade("Avenida Pacaembu", 2000, 200, 1000, 2000);
-        posicoes[37] = new Propriedade("Rua Augusta", 2100, 210, 1050, 2100);
-        posicoes[38] = new CartaDeSorte("Sorte/Reves");
-        posicoes[39] = new Propriedade("Avenida Europa", 2200, 220, 1100, 2200);
+        try (BufferedReader br = new BufferedReader(new FileReader("/home/android/devFolder/POO22024/BancoIF/src/tabuleiro.csv"))) {
+            String linha;
+            int index = 0;
+            while ((linha = br.readLine()) != null) {
+                String[] dados = linha.split(",");
+                String tipo = dados[0];
+                switch (tipo) {
+                    case "Propriedade":
+                        posicoes[index] = new Propriedade(dados[1], Integer.parseInt(dados[2]), Integer.parseInt(dados[3]), Integer.parseInt(dados[4]), Integer.parseInt(dados[5]));
+                        break;
+                    case "CartaDeSorte":
+                        posicoes[index] = new CartaDeSorte(dados[1]);
+                        break;
+                    case "ImpostoDeRenda":
+                        posicoes[index] = new ImpostoDeRenda(dados[1]);
+                        break;
+                    case "Companhia":
+                        posicoes[index] = new Companhia(dados[1], Integer.parseInt(dados[2]), Integer.parseInt(dados[3]));
+                        break;
+                    case "Cadeia":
+                        posicoes[index] = new Cadeia(dados[1]);
+                        break;
+                    case "LucrosOuDividendos":
+                        posicoes[index] = new LucrosOuDividendos(dados[1]);
+                        break;
+                    default:
+                        posicoes[index] = new Posicao(dados[1], dados[2]);
+                        break;
+                }
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Posicao getPosicao(int index) {
