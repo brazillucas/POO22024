d4d8a8fccb052f28dcf941977a2dbcb9dc2267f7
Android <l.cardoso64@hotmail.com>
2025-02-03 02:19:44 -0300

    Refatora a classe Entrada para melhorar a solicitação de entrada, removendo o uso de try-with-resources e ajustando a lógica de captura de senha e email.

--- OBLibrary/src/Entrada.java
+++ OBLibrary/src/Entrada.java
@@ -4,31 +4,30 @@ import java.util.Scanner;
 public class Entrada {
 
     public static String solicitarEntradaValida(String mensagem, String regex, String mensagemErro) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            while(true) {
-                System.out.print(mensagem);
-                String entrada = scanner.nextLine();
-                if (entrada.matches(regex)) {
-                    return entrada;
-                } else {
-                    System.out.printf("%s! Tente novamente.\n", mensagemErro);
-                }
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print(mensagem);
+            String entrada = scanner.nextLine();
+            if (entrada.matches(regex)) {
+                return entrada;
+            } else {
+                System.out.printf("%s! Tente novamente.\n", mensagemErro);
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao solicitar entrada.");
-            return null;
         }
     }
 
     public static String solicitarSenha() {
+        Scanner scanner = new Scanner(System.in);
         while(true) {
             Console console = System.console();
-            if (console == null) {
-                System.out.println("Console indisponível");
-                System.exit(1);
+            String senha;
+            if (console != null) {
+                char[] senhaArray = console.readPassword("Senha: ");
+                senha = new String(senhaArray);
+            } else {
+                System.out.print("Senha: ");
+                senha = scanner.nextLine();
             }
-            char[] senhaArray = console.readPassword("Senha: ");
-            String senha = new String(senhaArray);
             if (senha.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{4,8}$")) {
                 return senha;
             } else {
@@ -38,6 +37,7 @@ public class Entrada {
     }
 
     public static String solicitarEmail() {
-        return solicitarEntradaValida("Digite o email do usuário: ", "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", "Email Inválido");
+        String email = solicitarEntradaValida("Digite o email do usuário: ", "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", "Email Inválido");
+        return email;
     }
 }
