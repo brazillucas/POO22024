132b95bdbf3a1087ce14218e1ee9bf13991714fe
Android <l.cardoso64@hotmail.com>
2025-02-03 23:13:42 -0300

    Remove documentação desnecessária da classe Professor para simplificar o código

--- OBLibrary/src/Professor.java
+++ OBLibrary/src/Professor.java
@@ -1,15 +1,3 @@
-
-
-/**
- * 4. Classe Professor (Herda de Usuario)
-    Responsabilidade: Representa um professor que pode realizar empréstimos.
-
-    Atributos Adicionais:
-    departamento (String): Departamento ao qual o professor está vinculado.
-    Métodos:
-    realizarEmprestimo(ObraLiteraria obra): Permite até 10 empréstimos simultâneos.
-    realizarDevolucao(ObraLiteraria obra): Registra a devolução de uma obra.
-    */
 public class Professor extends Usuario{
     private String departamento;
 
