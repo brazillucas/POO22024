1f30752b3477cb6f8df119f49cde1b130e83d637
Android <l.cardoso64@hotmail.com>
2025-02-03 23:13:15 -0300

    Refatora a classe Usuario para gerenciar empréstimos realizados e melhorar a lógica de validação de empréstimos

--- OBLibrary/src/Usuario.java
+++ OBLibrary/src/Usuario.java
@@ -1,31 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 2. Classe Usuario (Classe Abstrata)
-    Responsabilidade: Representa um usuário genérico do sistema. Será usada como base para as subclasses específicas.
-
-    Atributos:
-    nome (String): Nome do usuário.
-    email (String): E-mail do usuário.
-    senha (String): Senha do usuário.
-    emprestimosAtivos (List<ObraLiteraria>): Lista de obras atualmente emprestadas ao usuário.
-    bloqueado (boolean): Indica se o usuário está bloqueado por atraso.
-    
-    Métodos:
-    login(String email, String senha): Verifica se o login é válido.
-    logout(): Realiza o logout do usuário.
-    verificarBloqueio(): Verifica se o usuário está bloqueado.
-    realizarEmprestimo(ObraLiteraria obra): Método abstrato para realizar empréstimos.
-    realizarDevolucao(ObraLiteraria obra): Método abstrato para realizar devoluções.
+ Responsabilidade: Representa um usuário genérico do sistema. Será usada como base para as subclasses específicas.
+ 
+ Atributos:
+ nome (String): Nome do usuário.
+ email (String): E-mail do usuário.
+ senha (String): Senha do usuário.
+ emprestimosAtivos (List<ObraLiteraria>): Lista de obras atualmente emprestadas ao usuário.
+ bloqueado (boolean): Indica se o usuário está bloqueado por atraso.
+ 
+ Métodos:
+ login(String email, String senha): Verifica se o login é válido.
+ logout(): Realiza o logout do usuário.
+ verificarBloqueio(): Verifica se o usuário está bloqueado.
+ realizarEmprestimo(ObraLiteraria obra): Método abstrato para realizar empréstimos.
+ realizarDevolucao(ObraLiteraria obra): Método abstrato para realizar devoluções.
  */
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Usuario {
     private String nome;
     private String email;
     private String senha;
     private int TipoUsuario = 0;
-    private List<Emprestimo> emprestimosAtivos;
+    private List<Emprestimo> emprestimosRealizados;
     private boolean bloqueado;
 
     public Usuario(String nome, String email, String senha, int TipoUsuario) {
@@ -33,7 +35,7 @@ public abstract class Usuario {
         this.email = email;
         this.senha = senha;
         this.TipoUsuario = TipoUsuario;
-        this.emprestimosAtivos = new ArrayList<>();
+        this.emprestimosRealizados = new ArrayList<>();
         this.bloqueado = false;
     }
 
@@ -49,39 +51,20 @@ public abstract class Usuario {
         return this.bloqueado;
     }
 
-    public void realizarDevolucao(ObraLiteraria obra) {
-        for (Emprestimo emprestimo : this.emprestimosAtivos) {
-            if (emprestimo.getObra().equals(obra)) {
-                emprestimo.setDevolucaoRealizada();
-                obra.atualizarQuantidade(1);
-                System.out.printf("A obra %s não está mais emprestada para %s.\n", emprestimo.getUsuario().getNome(), obra.getNome(), this.nome);
-                return;
-            }
-        }
-
-        System.out.printf("%s não possui a obra %s emprestada.\n", this.nome, obra.getTitulo());
-    }
-
-    public void realizarEmprestimo(ObraLiteraria obra) {
-        if (!validacaoEmprestimo(obra)) {
-            return;
-        }
-        Emprestimo novoEmprestimo = new Emprestimo(this, obra);
-        this.emprestimosAtivos.add(novoEmprestimo);
-        System.out.println("Empréstimo realizado com sucesso.");
-        System.out.printf("Novo livro emprestado: %s\n", obra.getTitulo());
-        obra.atualizarQuantidade(-1);
-    }
-
     public boolean validacaoEmprestimo(ObraLiteraria obra) {
-        if (this.bloqueado) {
+        if (this.verificarBloqueio()) {
             System.out.printf("%s está bloqueado por atraso.\n", this.nome);
             return false;
         }
 
-        for (Emprestimo emprestimo : this.emprestimosAtivos) {
-            if (emprestimo.getObra().equals(obra)) {
+        for (Emprestimo emprestimo : this.emprestimosRealizados) {
+            if (emprestimo.getObra().equals(obra) && emprestimo.getEmprestimoAtivo()) {
                 System.out.printf("%s já possui a obra %s emprestada.\n", this.nome, obra.getTitulo());
+                return false;
+            }
+            if (emprestimo.getDataDevolucaoPrevista().isBefore(LocalDate.now()) && emprestimo.getEmprestimoAtivo()) {
+                System.out.printf("%s possui a obra %s emprestada em atraso.\n", this.nome, obra.getTitulo());
+                this.setBloqueado(true);
                 return false;
             }
         }
@@ -91,16 +74,25 @@ public abstract class Usuario {
             return false;
         }
 
-        if (this.TipoUsuario == 1 && this.emprestimosAtivos.size() >= 2) {
+        return verificarLimiteEmprestimos();
+    }
+
+    public boolean verificarLimiteEmprestimos() {
+        int empAtivos = 0;
+        for (Emprestimo emprestimo : this.emprestimosRealizados) {
+            if (emprestimo.getEmprestimoAtivo()) {
+                empAtivos++;
+            }
+        }
+        if (this.TipoUsuario == 1 && empAtivos >= 2) {
             System.out.printf("%s já possui 2 obras emprestadas.\n", this.nome);
             return false;
         }
 
-        if (this.TipoUsuario == 2 && this.emprestimosAtivos.size() >= 10) {
+        if (this.TipoUsuario == 2 && empAtivos >= 10) {
             System.out.printf("%s já possui 10 obras emprestadas.\n", this.nome);
             return false;
         }
-
         return true;
     }
 
@@ -120,33 +112,33 @@ public abstract class Usuario {
         return this.TipoUsuario;
     }
 
-    public List<Emprestimo> getEmprestimosAtivos() {
-        return this.emprestimosAtivos;
+    public List<Emprestimo> getEmprestimosRealizados() {
+        return this.emprestimosRealizados;
     }
 
     public void exibirObrasEmprestadas() {
         boolean emprestimosAbertos = false;
-        for (Emprestimo emprestimo : this.emprestimosAtivos) {
-            if (emprestimo.getDataDevolucaoRealizada() == null) {
+        for (Emprestimo emprestimo : this.emprestimosRealizados) {
+            if (emprestimo.getEmprestimoAtivo()) {
                 emprestimosAbertos = true;
             }
         }
 
-        if (this.emprestimosAtivos.isEmpty() || !emprestimosAbertos) {
+        if (this.emprestimosRealizados.isEmpty() || !emprestimosAbertos) {
             System.out.printf("%s não possui obras emprestadas.\n", this.nome);
             return;
         }
         
 
         System.out.printf("Obras emprestadas para %s:\n", this.nome);
-        for (Emprestimo emprestimo : this.emprestimosAtivos) {
+        for (Emprestimo emprestimo : this.emprestimosRealizados) {
             if (emprestimo.getDataDevolucaoRealizada() == null) {
-                System.out.printf("%d | Título: %s\n", emprestimo.getObra().getId(), emprestimo.getObra().getTitulo());
+                System.out.printf("%2d | Título: %s\n", emprestimo.getObra().getId(), emprestimo.getObra().getTitulo());
             }
         }
     }
 
-    public boolean isBloqueado() {
+    public boolean estaBloqueado() {
         return this.bloqueado;
     }
 
@@ -154,5 +146,18 @@ public abstract class Usuario {
         this.bloqueado = bloqueado;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
 
 }
