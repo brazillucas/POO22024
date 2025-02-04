57bfa541ba8096e1763c8f81b1c7c70aa840be1b
Android <l.cardoso64@hotmail.com>
2025-02-03 23:12:35 -0300

    Refatora o loop principal do sistema para melhorar a estrutura do menu e a lógica de login

--- OBLibrary/src/Main.java
+++ OBLibrary/src/Main.java
@@ -2,99 +2,38 @@ public class Main {
 
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
+        biblioteca.carregarDados();
+
         Menu menu = new Menu();
-
-        while(true) {
+        Entrada.limparTela();
+        
+        // Loop principal do sistema
+        while (true) {
             menu.exibirMenuPrincipal();
-            int opcao = Integer.parseInt(Entrada.solicitarEntradaValida("Escolha uma opção: ", "[0-2]", "Opção inválida"));
+            int opcao = Integer.parseInt(Entrada.solicitarEntradaValida("Escolha uma opção: ", "[0-4]", "Opção inválida"));
 
-            if (opcao == 0) {
-                System.out.println("Salvando relatórios...");
-                biblioteca.salvarDados();
-                System.out.println("Encerrando sistema...");
-                break;
-            }
-
-           if (opcao == 1 && biblioteca.getQuantUsuarios() == 0) {
-                System.out.println("Nenhum usuário cadastrado.");
-                continue;
-            }
-
-            if (opcao == 1) {
-                realizarLogin(biblioteca, menu);
+            switch (opcao) {
+                case 0:
+                    biblioteca.fechamentoSistema();
+                    System.out.println("Encerrando sistema...");
+                    return;
+                case 1:
+                    if (biblioteca.getQuantUsuarios() == 0) {
+                        System.out.println("Nenhum usuário cadastrado.");
+                        Entrada.finalizarFuncao();
+                    } else {
+                        biblioteca.realizarLogin(menu);
+                        Entrada.finalizarFuncao();
+                    }
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    Entrada.aguardarEnter();
+                    Entrada.limparTela();
+                    break;
             }
         }
 
     }
-
-    private static void realizarLogin(Biblioteca biblioteca, Menu menu) {
-        String email = Entrada.solicitarEmail();
-        String senha = Entrada.solicitarSenha();
-
-        Usuario usuario = biblioteca.loginUsuario(email, senha);
-        int opcao;
-
-        while (biblioteca.getUsuarioLogado() != null) {
-            if (usuario instanceof Aluno || usuario instanceof Professor) {
-                menu.exibirMenuUsuario(usuario);
-                opcao = Integer.parseInt(Entrada.solicitarEntradaValida("Escolha uma opção: ", "^[0-4]$", "Opção inválida"));
-                
-                if (opcao == 0) {
-                    biblioteca.logoutUsuario();
-                } else if (opcao == 1) {
-                    ObraLiteraria obra = biblioteca.selecionarObra();
-                    if (obra != null) {
-                        biblioteca.getUsuarioLogado().realizarEmprestimo(obra);
-                    } else {
-                        System.out.println("Obra não encontrada.");
-                        System.out.println("Retornando ao menu...");
-                    }
-                } else if (opcao == 2) {
-                    biblioteca.getUsuarioLogado().exibirObrasEmprestadas();
-                    int idObra = Integer.parseInt(Entrada.solicitarEntradaValida("Digite o ID da obra que deseja devolver: ", "^[0-9]+$", "ID inválido"));
-
-                    ObraLiteraria obra = biblioteca.buscarObra(idObra);
-                    if (obra != null) {
-                        usuario.realizarDevolucao(obra);
-                    } else {
-                        System.out.println("Obra não encontrada.");
-                        System.out.println("Retornando ao menu...");
-                    }
-                } else if (opcao == 3) {
-                    biblioteca.getUsuarioLogado().exibirObrasEmprestadas();
-                }
-
-            } else if (usuario instanceof Bibliotecario) {
-                menu.exibirMenuBibliotecario(usuario);
-
-                opcao = Integer.parseInt(Entrada.solicitarEntradaValida("Escolha uma opção: ", "^[0-3]$", "Opção inválida"));
-
-                if (opcao == 0) {
-                    biblioteca.logoutUsuario();
-                } else if (opcao == 1) {
-                    ((Bibliotecario) usuario).cadastrarUsuario(biblioteca);
-                } else if (opcao == 2) {
-                    biblioteca.getUsuarioLogado().exibirObrasEmprestadas();
-                    int idObra = Integer.parseInt(Entrada.solicitarEntradaValida("Digite o ID da obra que deseja registrar a devolução: ", "^[0-9]+$", "ID inválido"));
-
-                    ObraLiteraria obra = biblioteca.buscarObra(idObra);
-                    if (obra != null) {
-                        ((Bibliotecario) usuario).registrarDevolucao(biblioteca.getUsuarioLogado(), obra);
-
-                    } else {
-                        System.out.println("Obra não encontrada.");
-                        System.out.println("Retornando ao menu...");
-                    }
-                } else if (opcao == 3) {
-                    System.out.println("Gerando relatórios...");
-                    ((Bibliotecario) usuario).gerarRelatorios(biblioteca);
-                }
-            }
-        }
-
-        System.out.println("E-mail ou senha incorretos.");
-    }
-
-
 
 }
