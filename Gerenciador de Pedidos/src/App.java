4ae1a952646d3e9cea6754dbb49375150c0f0992
Android <l.cardoso64@hotmail.com>
2025-02-17 00:13:18 -0300

    Atualiza a classe App para integrar o GerenciadorPedidos e simplificar o fluxo do menu

--- Gerenciador de Pedidos/src/App.java
+++ Gerenciador de Pedidos/src/App.java
@@ -1,46 +1,27 @@
-import java.time.LocalDate;
 
 public class App {
     public static void main(String[] args) {
-        
-        SistemaDeLogin sistemaLogin = new SistemaDeLogin();
+        GerenciadorPedidos gerenciador = new GerenciadorPedidos();
 
         Menu menu = new Menu();
+        
+        String opc;
         menu.exibirMenuIncial();
-
-        String opc = Entrada.solicitarEntradaValida("Digite a opção desejada: ", "^[0-1]$", "Opção inválida. Digite apenas números.");
-
+        opc = Entrada.solicitarEntradaValida("Digite a opção desejada: ", "^[1-2]$", "Opção inválida. Digite apenas números");
+        
         while (!opc.equals("2")) {
             switch (opc) {
                 case "1":
-                    menu.exibirMenuPrincipal();
-                    break;
-                case "2":
-                    System.out.println("Saindo...");
+                    // Acessar o sistema
+                    Entrada.limparTela();
+                    gerenciador.iniciarSistema();
                     break;
             }
-            opc = Entrada.solicitarEntradaValida("Digite a opção desejada: ", "^[0-1]$", "Opção inválida. Digite apenas números.");
+            opc = Entrada.solicitarEntradaValida("Digite a opção desejada: ", "^[1-2]$", "Opção inválida. Digite apenas números");
         }
-
+        
         // Fechar o sistema
-        System.out.println("Saindo do sistema...");
-        System.exit(0);
-
-
-        
-        // Adiciona o administrador ao sistema de login
-        // sistemaLogin.adicionarAdministrador(administrador);
-
-        // Autentica o usuário
-        // administrador = sistemaLogin.autenticar(usuario, senha);
-        
-        // Cria um gerenciador de pedidos
-        // GerenciadorPedidos gerenciador = new GerenciadorPedidos(administrador);
-        
-        // Cria um setor
-        // Setor setor = new Setor(1, "Setor 1");
-    }
-
-    public static void iniciarSistema() {
+        System.out.println("Saindo...");
+        Entrada.aguardarEnter();
     }
 }
